462550e69a02a126e39df99b8ac78e8ba5dfd27a
Anil Kumar <sendkumaranil@gmail.com>
2017-12-27 17:37:12 +0530

    Add files via upload

--- CustomHashMap.java
+++ CustomHashMap.java
@@ -1,4 +1,4 @@
-package com.custom.map;
+package com.javaprograms;
 
 import javax.naming.OperationNotSupportedException;
 
@@ -14,11 +14,12 @@ class Entry<K,V>{
 }
 
 public class CustomHashMap<K,V> {
-
+	
 	private Entry<K,V> head;
 	private int capacity;
 	private Entry[] table;
 	private boolean isNullKeyAvailable=false;
+	
 	public CustomHashMap(){
 		table=new Entry[10]; //default size map
 	}
@@ -38,7 +39,7 @@ public class CustomHashMap<K,V> {
 		return false;
 	}	
 	
-	public V put(K key,V value){
+public V put(K key,V value){
 		
 		
 		int index;
@@ -49,6 +50,7 @@ public class CustomHashMap<K,V> {
 					throw new OperationNotSupportedException("Null key allowed only once!!");
 				} catch (OperationNotSupportedException e1) {					
 					e1.printStackTrace();
+					return null;
 				}
 			}else{
 				isNullKeyAvailable=true;				
@@ -60,13 +62,14 @@ public class CustomHashMap<K,V> {
 			index=hash(key);			
 		}
 		//Create new entry here with key value pair
-		Entry<K,V> newEntry=new Entry<>(key,value,null);
+		Entry<K,V> newEntry=new Entry<K,V>(key,value,null);
 		
 		//get the firts node from table index
 		Entry<K,V> headNode=table[index];
 		//if there is no first node
 		if(headNode==null){
 			headNode=newEntry;
+			table[index]=headNode;
 		}else{
 			Entry<K,V> curr=headNode;
 			while(curr.next != null){
@@ -78,63 +81,109 @@ public class CustomHashMap<K,V> {
 				curr=curr.next;
 			}
 			curr.next=newEntry;
+			table[index]=curr;
 		}		
 		return null;
 	}
-	
-	public V get(K key){
-		int index;
-		if(checkNullKey(key)){
-			index=0;
-		}else{
-			index=hash(key);
-		}
-		Entry<K,V> curr=table[index];
-		while(curr!=null){
-			if(curr.key.equals(key)){
-				V val=curr.value;
-				return val;
-			}
-			curr=curr.next;
-		}		
-		return null;
+
+public V get(K key){
+	int index;
+	if(checkNullKey(key)){
+		index=0;
+	}else{
+		index=hash(key);
 	}
-	
-	public V remove(K key){
-		int index;
-		if(checkNullKey(key)){
-			index=0;
-		}else{
-			index=hash(key);
+	Entry<K,V> curr=table[index];
+	while(curr!=null){
+		//if key is null but null key is available in map then
+		if(key == null && curr != null){
+			V val=curr.value;
+			return val;
+		}else if(key == null && curr == null){
+			return null;
 		}
-		Entry<K,V> curr=table[index];
-		Entry<K,V> prev=null;
-		while(curr!=null){
-			//if first node delete
-			if(curr.key.equals(key) && prev ==null){
-				V val=curr.value;
-				curr=curr.next;
-				return val;
-			}else if(curr.key.equals(key)){
-				V val=curr.value;
-				prev.next = curr.next;
-				return val;
-			}
-			prev=curr;
-			curr=curr.next;
+		
+		if(curr.key.equals(key)){
+			V val=curr.value;
+			return val;
 		}
-		return null;
+		curr=curr.next;
+	}		
+	return null;
+}
+
+public V remove(K key){
+	int index;
+	if(checkNullKey(key)){
+		index=0;
+	}else{
+		index=hash(key);
 	}
-	
-	public void print(){
-		for(int i=0;i<table.length; i++){
-			if(table[i]!=null){
-				Entry<K,V> entry=table[i];
-				while(entry != null){
-					System.out.println("Key:"+entry.key+" Value:"+entry.value);
-					entry=entry.next;
-				}
+	Entry<K,V> curr=table[index];
+	Entry<K,V> head=table[index];
+	Entry<K,V> prev=null;
+	while(curr!=null){
+		//if key is null but null key is available in map then
+		if(key==null  && curr != null){
+			V val=curr.value;
+			table[index]=null;
+			return val;
+		}else if(key == null && curr == null){
+			return null;
+		}
+		
+		//if first node delete
+		if(curr.key.equals(key) && prev ==null){
+			V val=curr.value;
+			head=curr.next;
+			table[index]=head;
+			return val;
+		}else if(curr.key.equals(key)){
+			V val=curr.value;
+			prev.next = curr.next;
+			table[index]=head;
+			return val;
+		}
+		prev=curr;
+		curr=curr.next;
+	}
+	return null;
+}
+public void print(){
+	for(int i=0;i<table.length; i++){
+		if(table[i]!=null){
+			Entry<K,V> entry=table[i];
+			while(entry != null){
+				System.out.println("Key:"+entry.key+" Value:"+entry.value);
+				entry=entry.next;
 			}
 		}
 	}
+}
+
+
+
+public static void main(String[] args) {
+	
+	
+	CustomHashMap<Integer,Integer> map=new CustomHashMap<Integer,Integer>();
+	map.put(1, 10);
+	map.put(2, 20);
+	map.put(3, 30);
+	map.put(4, 40);
+	map.put(null, 50);
+	//map.put(null, 60); //check null twice as key
+	
+	map.print();
+	
+	System.out.println(map.get(3));
+	
+	System.out.println(map.remove(null));
+	
+	map.print();
+	
+	System.out.println(map.get(null));
+}
+
+
 }
